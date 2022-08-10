4202532ea0dd49de173261656d90ee335f3b97ae
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2022-08-10 13:12:33 -0300

    initial

---
+++ src/main/java/me/digo/academia/academiadigital/AcademiaDigitalApplication.java
@@ -0,0 +1,13 @@
+package me.digo.academia.academiadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/digo/academia/academiadigital/AcademiaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.digo.academia.academiadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
