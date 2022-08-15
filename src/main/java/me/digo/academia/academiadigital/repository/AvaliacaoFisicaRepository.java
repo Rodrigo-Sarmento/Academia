322895d9391fe0298d9c6a6d5a27e7989a22373e
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2022-08-15 15:06:33 -0300

    add: repository

---
+++ src/main/java/me/digo/academia/academiadigital/repository/AvaliacaoFisicaRepository.java
@@ -0,0 +1,10 @@
+package me.digo.academia.academiadigital.repository;
+
+import me.digo.academia.academiadigital.entity.AvaliacaoFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
+
+}
